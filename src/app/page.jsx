c0807c26a5cd2f1a5703522c45e1c27c2808e155
china akther <chinaakther05@gmail.com>
2026-01-13 16:27:19 -0800

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" ">
+     
+    <button className="btn btn-primary">test button </button>
+
+    </div>
+  );
+}
---
+++ src/components/layouts/Logo.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <Link href={"/"} className='flex items-center gap-1'>
+            <Image alt='logo-our-products' src={'/images.png'} width={60} height={50} />
+            <h2 className='text-xl font-bold'>Smart <span className='text-emerald-900'>Shop</span> </h2>
+        </Link>
+    );
+};
+
+export default Logo;
